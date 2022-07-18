ad6c36c19b4ca9dc31ad546f2e7c7ac125885a20
John Higgins <johnmatthiggins@gmail.com>
2022-07-17 18:56:35 -0700

    It's not compiling ugh

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 use ansi_term::Colour;
+use git2::Repository;
+use git2::Error;
 use std::env;
+
+enum GitStatus {
+    Clean(String),
+    Dirty(String),
+}
 
 fn main() -> std::io::Result<()> {
     let cwd = env::current_dir()?;
     let path = cwd
-        .to_str()
-        .map(|p| Colour::Red.paint(p));
+        .to_str();
 
     if let Some(s) = path {
-        println!("{}", s);
+        match git_status(&s.to_string()) {
+            Ok(b) => println!("({}) {} $", b, s),
+            Err(_) => println!("{} $", s),
+        }
     }
 
     Ok(())
+}
+
+fn git_status(cwd: &String) -> Result<String, Error> {
+    let repo = Repository::open(cwd);
+
+    repo.and_then(|r| r
+        .head()
+        .and_then(|h| h
+            .name()
+            .to_string()))
 }
